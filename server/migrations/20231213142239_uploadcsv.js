b89e1fa54b932cd3f01bf981e76e1d57fda0c527
LaThanhTrong <lathanhtrong32200@gmail.com>
2023-12-13 21:49:33 +0700

    setup database

---
+++ server/migrations/20231213142239_uploadcsv.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('uploadcsv', table => {
+        table.increments('id').primary()
+        table.string('filename').notNullable().unique()
+        table.string('destination').notNullable()
+        table.timestamps(true, true)
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('uploadcsv')
+};
